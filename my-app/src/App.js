e5a9354d4ee279736fc8327ed268bcc9f7cfe353
Mitra <mitra.toghtamesh.79@gmail.com>
2022-03-05 02:14:11 +0330

    Added HTML structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -7,15 +8,16 @@ export default function App() {
         <
         div className = "container" >
         <
-        h1 > Weather App < /h1>{" "} <
+        Weather / >
+        <
         footer >
         This project was coded by Mitra Toghtamesh and is { " " } <
         a href = "https://github.com/mitra0079/weatherReact"
         target = "_blank" >
         open - sourced on Github { " " } <
         /a>{" "} <
-        /footer> <
-        /div> <
+        /footer>{" "} <
+        /div>{" "} <
         /div>
     );
 }
