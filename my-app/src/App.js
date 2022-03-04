25923c8176a50b818a6f634774afd14090317564
Mitra <mitra.toghtamesh.79@gmail.com>
2022-03-05 00:22:48 +0330

    Added the Github repo URL

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-    return <div className = "App" > Weather App < /div>;
+    return ( <
+        div className = "App" >
+        <
+        h1 > Weather App < /h1> <
+        footer >
+        This project was coded by Mitra Toghtamesh and is { " " } <
+        a href = "https://github.com/mitra0079/weatherReact"
+        target = "_blank" >
+        open - sourced on Github { " " } <
+        /a>{" "} <
+        /footer>{" "} <
+        /div>
+    );
 }
