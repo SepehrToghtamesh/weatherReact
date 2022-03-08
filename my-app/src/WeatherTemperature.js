33c74b8eaf5d5eba266b1b68bca2d6548a8e0e23
Mitra <mitra.toghtamesh.79@gmail.com>
2022-03-08 21:02:40 +0330

    Added Unit Conversion

---
+++ my-app/src/WeatherTemperature.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+    const [unit, setUnit] = useState("celsius");
+
+    function convertToFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+    function convertToCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius");
+    }
+
+    function fahrenheit() {
+        return (props.celsius * 9) / 5 + 32;
+    }
+    if (unit === "celsius") {
+        return ( <
+            div className = "WeatherTemperature" >
+            <
+            span className = "temperature" > { Math.round(props.celsius) } < /span>{" "} <
+            span className = "unit" > { " " }°
+            C | { " " } <
+            a href = "/"
+            onClick = { convertToFahrenheit } > { " " }°
+            F { " " } <
+            /a>{" "} <
+            /span>{" "} <
+            /div>
+        );
+    } else {
+        return ( <
+            div className = "WeatherTemperature" >
+            <
+            span className = "temperature" > { Math.round(fahrenheit()) } < /span>{" "} <
+            span className = "unit" > { " " } <
+            a href = "/"
+            onClick = { convertToCelsius } > { " " }°
+            C { " " } <
+            /a>{" "} |
+            °F { " " } <
+            /span>{" "} <
+            /div>
+        );
+    }
+}
