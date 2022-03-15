4141692001b4816e12a407183e4efbb829d3245b
Mitra <98425360+mitra0079@users.noreply.github.com>
2022-03-15 18:48:06 +0330

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,17 @@ import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
-    return ( <
-        div className = "App" >
-        <
-        div className = "container" >
-        <
-        Weather defaultCity = "New York" / >
-        <
-        footer >
-        This project was coded by Mitra Toghtamesh and is { " " } <
-        a href = "https://github.com/mitra0079/weatherReact"
-        target = "_blank"
-        rel = "noreferrer" >
-        open - sourced on Github { " " } <
-        /a>{" "} <
-        /footer>{" "} <
-        /div>{" "} <
-        /div>
+    return ( 
+        <div className = "App" >
+          <div className = "container" >
+             <Weather defaultCity = "New York" / >
+             <footer >
+                This project was coded by Mitra Toghtamesh and is { " " }
+                <a href = "https://github.com/mitra0079/weatherReact" target = "_blank" rel = "noreferrer" >
+                  open - sourced on Github
+                </a>
+             </footer>
+          </div>
+        </div>
     );
 }
