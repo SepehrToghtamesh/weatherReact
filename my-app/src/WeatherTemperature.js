1fa2dac5b89f2cb5bc1eaf439f733803ea6ad590
Mitra <98425360+mitra0079@users.noreply.github.com>
2022-03-15 20:39:46 +0330

    Update WeatherTemperature.js

--- my-app/src/WeatherTemperature.js
+++ my-app/src/WeatherTemperature.js
@@ -17,32 +17,19 @@ export default function WeatherTemperature(props) {
         return (props.celsius * 9) / 5 + 32;
     }
     if (unit === "celsius") {
-        return ( <
-            div className = "WeatherTemperature" >
-            <
-            span className = "temperature" > { Math.round(props.celsius) } < /span>{" "} <
-            span className = "unit" > { " " }°
-            C | { " " } <
-            a href = "/"
-            onClick = { convertToFahrenheit } > { " " }°
-            F { " " } <
-            /a>{" "} <
-            /span>{" "} <
-            /div>
+        return ( 
+            <div className = "WeatherTemperature" >
+              <span className = "temperature" > { Math.round(props.celsius) } </span> 
+              <span className = "unit" > °C | <a href = "/" onClick = { convertToFahrenheit } >°F </a></span>
+            </div>
         );
     } else {
-        return ( <
-            div className = "WeatherTemperature" >
-            <
-            span className = "temperature" > { Math.round(fahrenheit()) } < /span>{" "} <
-            span className = "unit" > { " " } <
-            a href = "/"
-            onClick = { convertToCelsius } > { " " }°
-            C { " " } <
-            /a>{" "} |
-            °F { " " } <
-            /span>{" "} <
-            /div>
+        return ( 
+            <div className = "WeatherTemperature" >
+              <span className = "temperature" >{ Math.round(fahrenheit()) }</span>
+              <span className = "unit" >
+                  <a href = "/" onClick ={ convertToCelsius } > °C</a> | °F </span>
+            </div>
         );
     }
 }
