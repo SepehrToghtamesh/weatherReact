0584ad3ab4eca4138cdcf755019be13abf7eb5a3
Mitra Toghtamesh <mitra.toghtamesh.79@gmail.com>
2022-03-18 19:24:55 +0330

    update

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,42 +4,49 @@ import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
 
-  useEffect(() => {
-    setLoaded(false);
-  }, [props.coordinates]);
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
 
-  function handleResponse(response) {
-    setForecast(response.data.daily);
-    setLoaded(true);
-  }
+    function handleResponse(response) {
+        setForecast(response.data.daily);
+        setLoaded(true);
+    }
 
-  if (loaded) {
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />{" "}
-                </div>
-              );
-            }
-          })}
-        </div>
-      </div>
-    );
-  } else {
-    let apiKey = "c4d31e8d10125bcf00a394387228d175";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    if (loaded) {
+        return ( <
+            div className = "WeatherForecast" >
+            <
+            div className = "row" > {
+                forecast.map(function(dailyForecast, index) {
+                    if (index < 5) {
+                        return ( <
+                            div className = "col"
+                            key = { index } >
+                            <
+                            WeatherForecastDay data = { dailyForecast }
+                            />{" "} <
+                            /div>
+                        );
+                    } else {
+                        return null;
+                    }
+                })
+            } <
+            /div> <
+            /div>
+        );
+    } else {
+        let apiKey = "c4d31e8d10125bcf00a394387228d175";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-    axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleResponse);
 
-    return null;
-  }
+        return null;
+    }
 }
